b016c0a9a65f45cebfa202e9e40cfeb3f857234d
woratat <84006033+woratat@users.noreply.github.com>
2021-07-30 13:01:41 +0700

    Edit name in Homework

--- Homework/script.js
+++ Homework/script.js
@@ -3,7 +3,7 @@ let profile = document.querySelector('.profile');
 function promiseCallApi(){
     axios.get('https://reqres.in/api/users/5').then(res =>{
         profile.src = res.data.data.avatar
-        document.getElementById('name').innerText = res.data.data.first_name
+        document.getElementById('name').innerText = res.data.data.first_name +" "+ response.data.data.last_name
         document.getElementById('email').innerText = res.data.data.email
     }).catch(err =>{
         console.log(`err`, err)
@@ -14,7 +14,7 @@ const testFunction = async() =>{
     try {
         let response = await axios.get('https://reqres.in/api/users/5')
         profile.src = response.data.data.avatar
-        document.getElementById('name').innerText = response.data.data.first_name
+        document.getElementById('name').innerText = response.data.data.first_name +" "+ response.data.data.last_name
         document.getElementById('email').innerText = response.data.data.email
     } catch (error) {
         console.log('error :>>', error)
